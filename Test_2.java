2003707379c71e7aa2bf855ae231c13c69c77643
Kadukova <uliakadukova@gmail.com>
2025-03-28 11:01:06 +0300

    Add files via upload

---
+++ Test_2.java
@@ -0,0 +1,9 @@
+public class Test_2 {
+    public static void main(String[]args) {
+        for (int x = 2; x < 10; x++) {
+            for (int y = 1; y < 11; y++)
+            System.out.println(x + "*" + y + "=" + x*y);
+            System.out.println();
+        }
+    }
+}
