2408c9575d4d2f38011559f3d3244a1b5474c4cf
Kadukova <uliakadukova@gmail.com>
2025-03-28 17:04:05 +0300

    Add files via upload

---
+++ multiplication_table.java
@@ -0,0 +1,49 @@
+public class multiplication_table {
+    public static void main(String[] args) {
+        for (int x = 2; x < 10; x++) {
+            int y = 1;
+            System.out.print(x + "*" + y + "=" + x * y + "\t");
+        }
+        System.out.println();
+        for (int x = 2; x < 10; x++) {
+            int y = 2;
+            System.out.print(x + "*" + y + "=" + x * y + "\t");
+        }
+        System.out.println();
+        for (int x = 2; x < 10; x++) {
+            int y = 3;
+            System.out.print(x + "*" + y + "=" + x * y + "\t");
+        }
+        System.out.println();
+        for (int x = 2; x < 10; x++) {
+            int y = 4;
+            System.out.print(x + "*" + y + "=" + x * y + "\t");
+        }
+        System.out.println();
+        for (int x = 2; x < 10; x++) {
+            int y = 5;
+            System.out.print(x + "*" + y + "=" + x * y + "\t");
+        }
+        System.out.println();
+        for (int x = 2; x < 10; x++) {
+            int y = 6;
+            System.out.print(x + "*" + y + "=" + x * y + "\t");
+        }
+        System.out.println();
+        for (int x = 2; x < 10; x++) {
+            int y = 7;
+            System.out.print(x + "*" + y + "=" + x * y + "\t");
+        }
+        System.out.println();
+        for (int x = 2; x < 10; x++) {
+            int y = 8;
+            System.out.print(x + "*" + y + "=" + x * y + "\t");
+        }
+        System.out.println();
+        for (int x = 2; x < 10; x++) {
+            int y = 9;
+            System.out.print(x + "*" + y + "=" + x * y + "\t");
+        }
+        System.out.println();
+    }
+}
